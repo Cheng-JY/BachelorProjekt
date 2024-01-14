4ac1658f420a8799416251fe4a28a51ecb0f72ba
Cheng-JY <chengjiaying.prc@gmail.com>
2024-01-14 19:16:36 +0100

    add changes

--- scikit_learn/DINO/02_Dinov2.py
+++ scikit_learn/DINO/02_Dinov2.py
@@ -1,8 +1,14 @@
 import torch
 import torchvision
 import torchvision.datasets as datasets
+import torchvision.transforms as transforms
 
 if __name__ == "__main__":
+    # 1. Load Data Set
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
 
-    cifar_trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=None)
-    cifar_testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=None)
+    cifar_trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    cifar_testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
