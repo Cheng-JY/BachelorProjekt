e5932842e7c268e7a13d4689dd692d8e934dcac7
Cheng-JY <chengjiaying.prc@gmail.com>
2024-05-20 19:30:08 +0200

    add function

--- Crowd_Layer/utils.py
+++ Crowd_Layer/utils.py
@@ -1,10 +1,17 @@
 import torch.nn.functional as F
 import numpy as np
 import torch
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def predict(p_class):
     y_pred = p_class.argmax(axis=1).numpy()
     return y_pred
+
+def perdict_annotation(logits_annot):
+    P_annot = F.softmax(logits_annot, dim=1)
+    P_annot = P_annot.detach().numpy()
+    return P_annot
 
 def predict_annotator_perf(P_class, logits_annot, return_confusion_matrix=False):
     n_annotators = logits_annot.shape[2]
@@ -20,3 +27,12 @@ def predict_annotator_perf(P_class, logits_annot, return_confusion_matrix=False)
 def seed_everything(seed=42):
     np.random.seed(seed)
     torch.manual_seed(seed)
+
+def plot_train_loss(path:str):
+    metrics = pd.read_csv(path)
+    metrics = metrics.groupby('epoch')['train_loss_epoch'].agg('mean')
+    metrics.plot()
+    plt.show()
+
+if __name__ == '__main__':
+    plot_train_loss('/Users/chengjiaying/BachelorProjekt/Crowd_Layer/lightning_logs/version_12/metrics.csv')
